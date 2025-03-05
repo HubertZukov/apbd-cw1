98e53e1add4c3a9c9e6f5c1fe98ae57844b6108c
Hubert Żukowski <s27926@pjwstk.edu.pl>
2025-03-05 18:32:47 +0100

    Modyfikacja 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Dobra zmiana");
+Console.WriteLine("COS TAM SEI ZMEINAI AAAA");
